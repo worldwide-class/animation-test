9557515be38b0a6f7ba142b34f53ac6ed1476970
ivallee <isaacvallee@gmail.com>
2017-11-22 21:11:48 -0800

    implement rockets

--- move/move.js
+++ move/move.js
@@ -10,20 +10,25 @@ var player = {
 	speed: 2
 };
 
+var rocket = {
+	x: 0,
+	y: 0,
+	speed: 2.5
+};
+
+var activeRockets = [];
+
 var LEFT = false; 
 var RIGHT = false;
 var UP = false; 
 var DOWN = false;
-var FIRE = false;
+var SPACE = false;
+var XKEY = false;
 
 
 ////// Arrow keys //////
 
 function move() {
-	
-	// console.log(player.x);
-	// console.log(canvas.width);gi
-
 
 	if(LEFT) { 
 
@@ -59,9 +64,10 @@ function move() {
 	}	
 }
 
+
 function laser() {
-	if (FIRE) {
-		fireLaser();
+	if (SPACE) {
+		fireRocket();
 	}
 }
 
@@ -70,7 +76,7 @@ document.onkeydown = function(e) {
 	if(e.keyCode == 38) UP = true;
 	if(e.keyCode == 39) RIGHT = true;
 	if(e.keyCode == 40) DOWN = true;
-	if(e.keyCode == 32) FIRE = true;
+	if(e.keyCode == 88) XKEY = true;
  }
 
 document.onkeyup = function(e) {
@@ -78,12 +84,23 @@ document.onkeyup = function(e) {
 	if(e.keyCode == 38) UP = false;
 	if(e.keyCode == 39) RIGHT = false;
 	if(e.keyCode == 40) DOWN = false;
-	if(e.keyCode == 32) FIRE = false;
+	if(e.keyCode == 88) XKEY = false;
+}
+
+document.onkeypress = function(e) {
+	if (e.keyCode == 32) {
+		fireRocket();
+	}
 }
 
 
 ////// other functions //////
 
+function blammo(){
+	if (XKEY) {
+		fireLaser();
+	}
+}
 
 //function to clear canvas
 function clearCanvas() {
@@ -102,6 +119,56 @@ function ship(x,y) {
     ctx.lineTo(x+15,y+50);
     ctx.lineTo(x-15,y+50);
     ctx.fill();
+}
+
+function fireRocket() {
+
+	// console.log(activeRockets)
+
+		rocket.x = player.x;
+		rocket.y = player.y;
+		var newRocketIndex = activeRockets.length + 1;
+
+		var newRocket = new drawRocket(newRocketIndex);
+		console.log(newRocket.y)
+		// newRocket.beginPath();
+		// newRocket.moveTo(rocket.x, rocket.y);
+		// newRocket.fillStyle = "red";
+		// newRocket.arc(rocket.x, rocket.y, 3, 0, Math.PI*2, true);
+		// newRocket.fill();
+		// newRocket.closePath();
+		activeRockets.push(newRocket);
+
+}
+
+function drawRocket(index) {
+
+	this.x = player.x;
+	this.y = player.y;
+	ctx.beginPath();
+	ctx.moveTo(player.x, player.y);
+	ctx.fillStyle = "red";
+	ctx.arc(player.x, player.y, 3, 0, Math.PI*2, true);
+	ctx.fill();
+	ctx.closePath();
+}
+
+function reDrawRockets(index) {
+	ctx.beginPath();
+	ctx.moveTo(activeRockets[index].x, activeRockets[index].y);
+	ctx.fillStyle = "red";
+	ctx.arc(activeRockets[index].x, activeRockets[index].y, 3, 0, Math.PI*2, true);
+	ctx.fill();
+	ctx.closePath();
+}
+
+function moveRockets() {
+
+	for (var i = 0; i < activeRockets.length; i++) {
+		activeRockets[i].y = activeRockets[i].y - 1;
+		reDrawRockets(i);
+	}
+
 }
 
 // Player ship's laser
@@ -128,4 +195,6 @@ function update() {
 	ship();
 	move();
 	laser();
+	blammo();
+	moveRockets();
 }
